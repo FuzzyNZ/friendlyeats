2234c24c33d13a9e71cbadb950ac640ff9b7587c
Steve Brodie <shbrodie@gmail.com>
2025-03-19 14:55:00 +1300

    gemini rework

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,24 +1,47 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+// import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+// import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+// import { environment } from '../environments/environment';
+// import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+// import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+// import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+// import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+// import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+// import { routes } from './app.routes';
+// import { provideRouter } from '@angular/router';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     importProvidersFrom(
+//       provideFirebaseApp(() => initializeApp(environment.firebase)),
+//       provideFirestore(() => getFirestore()),
+//       provideAuth(() => getAuth()),
+//       provideFunctions(() => getFunctions()),
+//       provideStorage(() => getStorage()),
+//       provideMessaging(() => getMessaging())
+//     ),
+//     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-fb8d8","appId":"1:364303774519:web:f0ed5e6a065bdbc9e260b2","storageBucket":"friendlychat-fb8d8.firebasestorage.app","apiKey":"AIzaSyBVfp4IK-d2Z93Y28P6CHfAGQLOhDC2Qbc","authDomain":"friendlychat-fb8d8.firebaseapp.com","messagingSenderId":"364303774519","measurementId":"G-NRTSZNZ6ER"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+//   ],
+// };
+
+import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-fb8d8","appId":"1:364303774519:web:f0ed5e6a065bdbc9e260b2","storageBucket":"friendlychat-fb8d8.firebasestorage.app","apiKey":"AIzaSyBVfp4IK-d2Z93Y28P6CHfAGQLOhDC2Qbc","authDomain":"friendlychat-fb8d8.firebaseapp.com","messagingSenderId":"364303774519","measurementId":"G-NRTSZNZ6ER"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
+    provideRouter(routes),
   ],
 };
